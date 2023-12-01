b402c02237b4e98cb5a178c4552053e910d26def
gd <gabrieldennis17@gmail.com>
2023-12-02 00:57:43 +1000

    fix: running solution

--- src/day1/main.c
+++ src/day1/main.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,141 +9,133 @@
 #define ASCI_NINE 57
 #define BUFFER_SIZE 256
 
-// Array of strings  to numbers
-
-const char *num_strs[] = {
-  "one", "two", 
-  "three", "four", "five",
-  "six", "seven", "eight", "nine"
-};
-
-
-char *first_char[] = { "on", "tw", "th", "fo", "fi", "si", "se",  "ei", "ni" };
-
-#define NUM_STR_MAP 0x1<<10 
-const char *str_map[NUM_STR_MAP];
-int  dig_map[NUM_STR_MAP];
-
-
 // Read in the files
-FILE * read_file(const char *f) {
+FILE *read_file(const char *f) {
   FILE *data_file = fopen(f, "r");
   if (data_file == NULL) {
     fprintf(stderr, "Error opening file %s\n", f);
     exit(EXIT_FAILURE);
-  } 
+  }
   return data_file;
 }
 
-#define TOKEN_SIZE 0x1<<16
-int num[TOKEN_SIZE]; 
+#define TOKEN_SIZE 0x1 << 16
+int num[TOKEN_SIZE];
 
-int hash(const char s, const char ss) {
-  return s + (ss * 20) - 2000;
+
+bool is_null_char(const char s) {
+  return  s == '\0' || s == '\n' || s == ' '; 
 }
 
+bool is_digit(const char s) { return s >= ASCI_ZERO && s <= ASCI_NINE; }
 
 int main(int argc, char **argv) {
-
-for (int i = 0; i < 9; i++) {
-    int h = hash(num_strs[i][0], num_strs[i][1]);
-    dig_map[h] = i + 1;
-    str_map[h] = num_strs[i];
-}
 
   FILE *data_file;
   const char *f;
   if (argc == 1) {
-    printf("No file specified\n");
-    f = "example.txt";
+    f = "day1_input.txt";
   } else {
     f = argv[1];
-    printf("Reading input: %s\n", f);
   }
 
+  data_file = read_file(f);
 
-  data_file =  read_file(f);
   char line[BUFFER_SIZE];
+
   int offset = 0;
 
   while (fgets(line, BUFFER_SIZE, data_file) != NULL) {
+
     int i = 0;
-    while(i < BUFFER_SIZE) {
-      
+    while( i < BUFFER_SIZE && line[i] != '\0') {
       char s = line[i];
-      if (s == '\n' || s == '\0' || s == ' ') {
+
+      if (is_null_char(s)) {
         num[offset++] = '\0';
         break;
       }
 
-
-      if (s >= ASCI_ZERO && s <= ASCI_NINE) {
+      if (is_digit(s)) {
         num[offset++] = (int)(s - ASCI_ZERO);
-         i++;
+        i++;
         continue;
       }
 
+      const char *ss;
+      int var = 0;
+      switch (s) {
+      case 'o':
+        ss = "one";
+        var = 1;
+        break;
+      case 't':
+        if (line[i + 1] == 'w') {
+          ss = "two";
+          var = 2;
+        } else {
+          ss = "three";
+          var = 3;
+        }
+        break;
+      case 'f':
+        if (line[i + 1] == 'o') {
+          ss = "four";
+          var = 4;
+        } else {
+          ss = "five";
+          var = 5;
+        }
+        break;
+      case 's':
+        if (line[i + 1] == 'i') {
+          ss = "six";
+          var = 6;
+        } else {
+          ss = "seven";
+          var = 7;
+        }
+        break;
+      case 'e':
+        ss = "eight";
+        var = 8;
+        break;
+      case 'n':
+        ss = "nine";
+        var = 9;
+        break;
+      }
 
-      for (int j = 0; j < 9; j++) {
-        if (s == first_char[j][0] && line[i + 1] == first_char[j][1]) {
-          int h = hash(s, line[i + 1]);
-          const char * v = str_map[hash(s, line[i + 1])];
-
-          int n = strlen(v);
-          int k = 0;
-          int match = 1;
-
-          while ((k + 2) < n) {
-
-
-            if (line[i + 2 + k]== '\n' || line[i + 2 +k] == '\0' || line[i+2+k] == ' ') {
-              num[offset++] = '\0';
-              break;
-            }
-
-            if (line[i + 2 + k] != v[k + 2]) {
-              match = 0;
-              i++;
-              break;
-            }
-          k++;
-          }
-
-          if (match == 1) {
-            i += n - 1;
-            num[offset++] = dig_map[h];
-            break;
-          }
+      int match = 1;
+      for (int j = 0; j < strlen(ss); j++) {
+        if (ss[j] != line[i + j]) {
+          match = 0;
+          i++;
+          break;
         }
       }
-      i++;
+
+      if (match == 1) {
+        num[offset++] = var;
+        i += strlen(ss) - 1;
+      }
     }
   }
-
 
   fclose(data_file);
 
   size_t size = 0;
-  size_t first = 0;
-  size_t second = 0;
+  size_t idx = 0;
+  size_t iidx = -1;
+
   for (int i = 0; i < offset; i++) {
     if (num[i] == '\0') {
-      if (second == 0) {
-        size += 10 * first + first;
-        first = 0;
-        continue;
-      } else {
-        size += (10 * first  +  second);
-        first = 0;
-        second = 0;
-        continue;
-      }
-    }
-    if (first == 0) {
-      first = num[i];
-    } else {
-      second = num[i];
+      idx = i;
+      size += (num[iidx + 1] * 10 + num[idx - 1]);
+      iidx = idx;
     }
   }
- printf("Sum: %zu\n", size);
+
+  size += (num[idx + 1] * 10 + num[offset - 1]);
+  printf("Total size: %zu\n", size);
 }
